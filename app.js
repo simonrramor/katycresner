9f2aa013750ea320878bf02b3af99094f4f8fa83
Simon Amor <simonrramor@gmail.com>
2017-12-26 12:58:56 +0000

    adding simple carousel, no tyles or anything yet but started

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-console.log('JS Loaded');
+var currentIndex = 0,
+  items = $('.container div'),
+  itemAmt = items.length;
 
+function cycleItems() {
+  var item = $('.container div').eq(currentIndex);
+  items.hide();
+  item.css('display','inline-block');
+}
 
-const $button = $('#button');
+var autoSlide = setInterval(function() {
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+}, 3000);
 
-$button.click(function() {
-  console.log('clicked');
+$('.next').click(function() {
+  clearInterval(autoSlide);
+  currentIndex += 1;
+  if (currentIndex > itemAmt - 1) {
+    currentIndex = 0;
+  }
+  cycleItems();
+});
+
+$('.prev').click(function() {
+  clearInterval(autoSlide);
+  currentIndex -= 1;
+  if (currentIndex < 0) {
+    currentIndex = itemAmt - 1;
+  }
+  cycleItems();
 });
